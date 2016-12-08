1ca3f708c0cf12f0fa8ec3a9cfc969256897424c
1am5teph <marson.stephanie@gmail.com>
2016-12-07 20:14:36 -0800

    created sql file

---
+++ build-tables.sql
@@ -0,0 +1,14 @@
+﻿CREATE SCHEMA homeless;
+
+DROP TABLE if EXISTS homeless.HistoryPitSummary;
+
+CREATE TABLE homeless.HistoryPitSummary (
+        "Table" VARCHAR(17) NOT NULL,
+        "Population" VARCHAR(32) NOT NULL,
+        "Sub-Population" VARCHAR(37) NOT NULL,
+        "Date" DATE NOT NULL,
+        "Value" VARCHAR(11)
+);
+
+COPY homeless.HistoryPitSummary FROM '/home/vagrant/proj/data/HistoryPitSummary.csv'
+	WITH(FORMAT CSV, HEADER);
