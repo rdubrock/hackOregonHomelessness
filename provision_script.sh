3148d2945fa80921dec16a4993d2867b4a2b97bf
Russell DuBrock <russell.dubrock@gmail.com>
2016-12-07 19:01:51 -0800

    reset vagrant scripts to class scripts

--- provision_script.sh
+++ provision_script.sh
@@ -1,52 +1,66 @@
 #!/bin/bash
 
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-apt-get update -y
-apt-get upgrade -y
 
 echo 'Setting up logfile'
 LOGFILE=/home/vagrant/provision_script.log
 echo > $LOGFILE
 
-#install firefox
-apt-get install -y firefox >> $LOGFILE
 
-#install postgrest, postgis, and pgadmin3
-apt-get install -y postgresql-9.5-postgis-2.2 pgadmin3 >> $LOGFILE
-echo 'Installing postgres'
 
-# Install pgRouting 2.1 package
-apt-get install -y postgresql-9.5-pgrouting >> $LOGFILE
-echo 'Installing pgRouting'
-
-#install python for postgres
-apt-get install -y postgresql-plpython3-9.5 >> $LOGFILE
-echo 'Installing Python'
-
-apt-get install -y python-pip >> $LOGFILE
-echo 'Installing python-pip'
-apt-get install -y dos2unix >> $LOGFILE
-echo 'Installing dos2unix'
-apt-get install -y postgis >> $LOGFILE
-echo 'Installing postgis'
-apt-get install -y unzip >> $LOGFILE
-echo 'Installing unzip'
-
+# Add postgis to list of trusted repositories
+echo 'Adding postgis to list of trusted repositories...'
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+apt-get update -y
+apt-get upgrade -y
 
-#cleanup
-apt-get autoremove -y
 
-pip install csvkit >> $LOGFILE
+# install apt packages
 
-#curl https://raw.githubusercontent.com/hackoregon/hack-university-database-engineering/master/Data/Crime%20Data/crimedata.csv -o /home/vagrant/proj/crimedataraw.csv >> $LOGFILE
+echo 'Installing Firefox...'
+apt-get install -y firefox >> $LOGFILE
+echo 'Installing PostgreSQL 9.5...'
+apt-get install -y postgresql-9.5-postgis-2.2 >> $LOGFILE
+echo 'Installing pgadmin3...'
+apt-get install -y pgadmin3 >> $LOGFILE
+echo 'Installing pgRouting 2.1...'
+apt-get install -y postgresql-9.5-pgrouting >> $LOGFILE
+echo 'Installing Python for Postgres...'
+apt-get install -y postgresql-plpython3-9.5 >> $LOGFILE
+echo 'Installing pip...'
+apt-get install -y python-pip >> $LOGFILE
+echo 'Installing dos2unix...'
+apt-get install -y dos2unix >> $LOGFILE
 
-su postgres < /home/vagrant/proj/database_creation.sh
+echo 'Cleaning up APT...'
+apt-get autoremove - >> $LOGFILE
+
+
+
+# install Python packages
+
+echo 'Installing csvkit...'
+pip install csvkit >> $LOGFILE         # for commands 'csvsql' and 'csvstat'
+
+
+
+# configure PostgreSQL
+
+echo 'Creating vagrant user in PostgreSQL...'
+su postgres -c 'psql -c "CREATE USER vagrant WITH CREATEUSER;"' >> $LOGFILE
+
+echo 'Creating vagrant database in PostgreSQL...'
+su postgres -c 'psql -c "CREATE DATABASE vagrant;"' >> $LOGFILE
+
+echo 'Running scripts as user vagrant...'
+su vagrant -c 'bash ~vagrant/proj/provision_script_vagrant.sh' >> $LOGFILE
+
 
 # change log file owner to vagrant
 chown vagrant:vagrant $LOGFILE
 
-echo 'run this on host machine: '
-echo 'vagrant plugin install vagrant-vbguest'
+
+
+echo 'Finished installing packages.'
+echo 'Run the following line on your host machine:'
+echo '$ vagrant plugin install vagrant-vbguest'
