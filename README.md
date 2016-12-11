e0d4654cb7aab7bb4b86765b236be7abe05f40fe
Troy Scott <troy_pdx@fastmail.fm>
2016-12-10 17:14:55 -0800

    added API's database access settings

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hackOregonHomelessness
+
+### Troy's Notes 10DEC
+
+1. Added port forwarding 8000/8001 to Vagrantfile
+2. Created 'tallyapp' for my open implementation of the API
+3. Created database user troys/troys with rights to access the database 'vagrant'
+4. Created Django superuser admin/admin123
+5. Default views from the host OS's web browser:
+    localhost:8001,
+    localhost:8001/admin
--- homelessness/settings.py
+++ homelessness/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = 'homelessness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# troys 10DEC removed sqllite3 config
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+# troys 10DEC added postgresql config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vagrant',
+        'USER': 'troys',
+        'PASSWORD': 'troys',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
